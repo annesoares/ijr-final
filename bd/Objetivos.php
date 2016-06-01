ccf1d94d9a49aef6756dff658f61249109cfd431
RaulNCunha <raulnasccunha@gmail.com>
2016-06-01 08:44:52 -0300

    Create Objetivos.php

---
+++ bd/Objetivos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<title>Objetivos Estratégicos</title>
+		<link rel = "stylesheet" href = "D:\Arquivos de Programas\wamp\www\Objetivos.css">
+    </head>
+	
+	<body>
+	
+	<!-- Menu -->
+	<header class="container">
+		<h1><img src="C:\Users\RaulNC\Desktop\logo ij.png" alt="Informática Júnior"></h1>
+			
+		<nav class="menu-opcoes">
+			<ul>
+				<li><a href="Homepage.php">Página Inicial</a></li>
+				<li><a href="Objetivos.php">Objetivos Estratégicos</a></li>
+				<li><a href="Membros.php">Membros</a></li>
+			</ul>
+		</nav>
+	</header>
+	<!-- Fim do Menu -->
+	
+	<!-- Tabela -->
+	    <h1 class="titulo">Objetivos Estratégicos</h1>
+    <!-- Fim da tabela -->
+	
+		<div id = "rodape">
+			&copy; Copyright Informática Júnior <?php print date("Y");?>
+		</div>
+	</body>
+</html>
